3bcd342350e1a1bfb9d5605fc5459dc50b46f497
Th. Androutsos <a8anassis@gmail.com>
2025-02-24 20:04:42 +0200

    inital commit

---
+++ src/gr/aueb/cf/schoolapp/Main.java
@@ -0,0 +1,23 @@
+package gr.aueb.cf.schoolapp;
+
+import java.awt.EventQueue;
+
+public class Main {
+	private final static LandingPage landingPage = new LandingPage();
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					
+					landingPage.setVisible(true);
+					landingPage.setLocationRelativeTo(null);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}

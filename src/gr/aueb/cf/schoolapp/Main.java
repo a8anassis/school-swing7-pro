6d653778efac230f05817703853b13b9d687195a
Th. Androutsos <a8anassis@gmail.com>
2025-02-25 19:56:53 +0200

    feat: Main class changes with new page instances. City class was created.

--- src/gr/aueb/cf/schoolapp/Main.java
+++ src/gr/aueb/cf/schoolapp/Main.java
@@ -6,6 +6,10 @@ public class Main {
 	private static final LandingPage landingPage = new LandingPage();
 	private static final LoginPage loginPage = new LoginPage();
 	private static final Dashboard dashboard =  new Dashboard();
+	private static final ViewTeachersPage viewTeachersPage = new ViewTeachersPage();
+	private static final TeacherView teacherView = new TeacherView();
+	private static final UpdateTeacherPage updateTeacherPage = new UpdateTeacherPage();
+	private static final InsertTeacherPage insertTeacherPage = new InsertTeacherPage();
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -20,6 +24,18 @@ public class Main {
 					
 					dashboard.setVisible(false);
 					dashboard.setLocationRelativeTo(null);
+					
+					viewTeachersPage.setVisible(false);
+					viewTeachersPage.setLocationRelativeTo(null);
+					
+					insertTeacherPage.setVisible(false);
+					insertTeacherPage.setLocationRelativeTo(null);
+					
+					teacherView.setVisible(false);
+					teacherView.setLocationRelativeTo(null);
+					
+					updateTeacherPage.setVisible(false);
+					updateTeacherPage.setLocationRelativeTo(null);
 					
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -38,6 +54,22 @@ public class Main {
 
 	public static Dashboard getDashboard() {
 		return dashboard;
+	}
+
+	public static ViewTeachersPage getViewTeachersPage() {
+		return viewTeachersPage;
+	}
+
+	public static TeacherView getTeacherView() {
+		return teacherView;
+	}
+
+	public static UpdateTeacherPage getUpdateTeacherPage() {
+		return updateTeacherPage;
+	}
+
+	public static InsertTeacherPage getInsertTeacherPage() {
+		return insertTeacherPage;
 	}
 	
 	
